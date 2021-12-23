de1436b254137c104fa4cdaeb29443ba2a6739c0
Kevin Rao <krao@Kevins-MacBook-Pro.local>
2021-12-23 03:30:25 -0600

    done simulating weather

--- engine/environment.py
+++ engine/environment.py
@@ -1,11 +1,12 @@
 import random
+import numpy.random
 
 class Environment:
 
 	# all rates are out of 1000
 
 	def __init__(self, sunrise_time, sunset_time, wind_avg, wind_volatility, geothermal_rate, dust_rate, snow_rate, snap_rate):
-		self.world_hour = 0 # Hour starts at 0, increments by 1 cycles mod 24
+		self.world_hour = 0 # Hour starts at 0, increments by 1
 		self.world_day = 0  # Day starts at 0, increments by 1 every 24 hours
 
 		self.sun_up = False
@@ -36,7 +37,10 @@ class Environment:
 			# mean should be an integer from -1000 to 1000
 			# spread should be an integer from 0 to 1000
 			assert not(mean < -1000 or mean > 1000 or spread < 0 or spread > 1000), "In rough_binom in env, got a mean of " + str(mean) + " and a spread of " + str(spread)
-			
+
+			binomial_n = (spread**2)*4
+			binomial_rv = numpy.random.binomial(n = binomial_n, p = 0.5)
+			return binomial_rv - binomial_n // 2 + mean
 
 
 
@@ -44,17 +48,16 @@ class Environment:
 
 		self.world_hour += 1
 
-		if self.world_hour == 24:
-			self.world_hour = 0
+		if self.world_hour % 24 == 0:
 			self.world_day += 1
 
 
 
 		# Sun ============
 
-		if self.world_hour == self.sunrise_time:
+		if self.world_hour % 24 == self.sunrise_time:
 			self.sun_up = True
-		elif self.world_hour == self.sunset_time:
+		elif self.world_hour % 24 == self.sunset_time:
 			self.sun_up = False
 
 
@@ -62,7 +65,19 @@ class Environment:
 
 		# Wind ===========
 
-		# if dust storm is incoming, wind volatility should favor increase, and if dust storm passes, wind should return to average
+		# if dust storm is incoming, wind should favor increase, and if dust storm passes, wind should return to average
+
+		
+
+		if self.active_storm and self.active_storm.storm_type == 1:
+			self.wind_speed += rough_binom(self.wind_avg - self.wind_speed + self.wind_volatility * 2, self.wind_volatility)
+		else:
+			self.wind_speed += rough_binom(self.wind_avg - self.wind_speed, self.wind_volatility)
+
+		if self.wind_speed < 0:
+			self.wind_speed = 0
+		elif self.wind_speed > 999:
+			self.wind_speed = 999
 
 
 
@@ -70,14 +85,54 @@ class Environment:
 
 		# if active storm is not None, check if we should end the current storm
 
-		# if no storms are active, roll the dice on each storm starting
+		if self.active_storm:
+			if self.world_hour >= self.active_storm.end_time:
+				self.active_storm = None
+		else:
+
+			# if no storms are active, roll the dice on each storm starting
+
+			storm_roll = random.randint(0, 999)
+
 
 			# if we start a storm, create a storm object with its starting and ending time and set active storm object
-			# Time from now to starting time should depend on the amount of forewarning we get - high for dust, medium for snow, low to none for snap
+
+			if storm_roll < self.dust_rate:
+				self.active_storm = Storm(self.world_hour, 1, self.dust_rate)
+			elif storm_roll < self.dust_rate + self.snow_rate:
+				self.active_storm = Storm(self.world_hour, 2, self.snow_rate)
+			elif storm_roll < self.dust_rate + self.snow_rate + self.snap_rate:
+				self.active_storm = Storm(self.world_hour, 3, self.snap_rate)
+
+
  
 
+class Storm:
+
+	def __init__(self, time, storm_type, rate):
+		self.storm_type = storm_type # 1 for dust, 2 for snow, 3 for snap
+		self.rate = rate
+
+		self.start_time = time
+
+		# Time from now to starting time should depend on the amount of forewarning we get - high for dust, medium for snow, low to none for snap
+		if self.storm_type == 1:
+			self.start_time += 6
+		elif self.storm_type == 2:
+			self.start_time += 3
+		else:
+			self.start_time += 0
 
 
+		duration_avg = self.rate // 10
+		duration = duration_avg + random.randint(-1 * (duration_avg // 10) - 1, duration_avg // 10 + 1)
+
+
+		self.end_time = self.start_time + duration + 1
+
+	def __str__(self):
+		output = 'Storm type ' + str(self.storm_type) + ' Starting ' + str(self.start_time) + ' Ending ' + str(self.end_time)
+		return output
 
 
 if __name__ == '__main__':
@@ -87,9 +142,11 @@ if __name__ == '__main__':
 							 wind_avg = 200,
 							 wind_volatility = 100,
 							 geothermal_rate = 200, # should be default
-							 dust_rate = 100,
-							 snow_rate = 30,
+							 dust_rate = 50,
+							 snow_rate = 20,
 							 snap_rate = 10
 	)
 
-	print summer_env.snow_rate
+	for i in range(600):
+		summer_env.update_conditions()
+		print summer_env.world_hour, summer_env.wind_speed, summer_env.active_storm
--- engine/test_wind_rw.py
+++ engine/test_wind_rw.py
@@ -1,4 +1,5 @@
 import random
+import numpy.random
 
 
 def rough_binom(mean, spread):
@@ -6,7 +7,11 @@ def rough_binom(mean, spread):
 	# spread should be an integer from 0 to 1000
 	assert not(mean < -1000 or mean > 1000 or spread < 0 or spread > 1000), "In rough_binom in env, got a mean of " + str(mean) + " and a spread of " + str(spread)
 
-			
+	binomial_n = (spread**2)*4
+	binomial_rv = numpy.random.binomial(n = binomial_n, p = 0.5)
+	print binomial_rv - binomial_n // 2 + mean
+	return binomial_rv - binomial_n // 2 + mean
+
 
 
 def run_sim(wind_avg, wind_volatility):
@@ -14,9 +19,25 @@ def run_sim(wind_avg, wind_volatility):
 
 	for i in range(100):
 		wind_speed = wind_speed + rough_binom(wind_avg - wind_speed, wind_volatility)
-		print wind_speed
+
+		if wind_speed < 0:
+			wind_speed = 0
+		elif wind_speed > 999:
+			wind_speed = 999
+
+		print wind_speed,
+
+		j = 0
+		while j < wind_speed:
+			if j == wind_avg:
+				print '0',
+			else:
+				print '=',
+			j += 10
+
+		print
 
 
 if __name__ == '__main__':
 
-	run_sim(200, 100)
+	run_sim(200, 200)
